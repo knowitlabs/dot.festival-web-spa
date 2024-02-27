af562cb93e35398a41ca7c449fdf3ad54f8f08ec
Alexander Vassbotn Røyne-Helgesen <alexander.github@phun-ky.net>
2024-02-27 16:19:58 +0100

    Update index.ts

--- src/pages/PracticalitiesPage/index.ts
+++ src/pages/PracticalitiesPage/index.ts
@@ -36,10 +36,10 @@ export const PracticalitiesPage = () => {
   })}
       </p>
       <p class="dot">
-      Registration deadline is <strong class="dot">17th of April</strong>.
+      Registration deadline is <strong class="dot">1st of April</strong>.
       </p>
       <p class="dot">
-      You may cancel before the 30th of April free of charge. After this, a cancellation fee up to the full price can be billed to your company if neither you nor we can find someone else to take your spot. If you wish to cancel or change anything else regarding your registration after submission, send a mail to <a href="mailto:dot.festival@knowit.eu" class="dot">dot.festival@knowit.eu</a>. Remember to consult with your leader before registration.
+      You may cancel before the 1st of April free of charge. After this, a cancellation fee up to the full price can be billed to your company if neither you nor we can find someone else to take your spot. If you wish to cancel or change anything else regarding your registration after submission, send a mail to <a href="mailto:dot.festival@knowit.eu" class="dot">dot.festival@knowit.eu</a>. Remember to consult with your leader before registration.
       </p>
       <p class="dot">
       Registration fee dot.festival: <strong class="dot">5000 NOK</strong>.
@@ -48,8 +48,8 @@ export const PracticalitiesPage = () => {
       The following is covered by this cost:
 </p>
 <ul class="dot">
-<li class="dot">Hotel Friday-Saturday (Single room)</li>
-<li class="dot">Lunch and dinner (Friday), breakfast and lunch (Saturday)</li>
+<li class="dot">Hotel Friday-Saturday (Single room/double if shared)</li>
+<li class="dot">Lunch and dinner (Friday), lunch (Saturday)</li>
 <li class="dot">Venue</li>
 </ul>
 <p class="dot">
