1dea0c24f23016b8c705c39a2d08aa7431618961
Karoline <karoline.vabo@knowit.no>
2025-05-06 15:29:00 +0200

    WIP - styling and content changes

--- src/components/page-section/Details/index.ts
+++ src/components/page-section/Details/index.ts
@@ -1,16 +1,27 @@
 const html = String.raw;
 
 import './styles/details.styl';
+import buttonBackground from '../../../assets/images/icons/extras/button.svg';
+import { Link } from '../../navigation/Link';
+import translate from '../../../lib/translate';
 
 export const Details = () => {
   return html`<section class="dot section details">
     <div class="dot container">
       <div class="dot content">
-        <span class="dot pretitle first">When?</span>
-        <h2 class="dot when">7 - 8 November 2025</h2>
-
-        <span class="dot pretitle second">Where?</span>
-        <h2 class="dot when">Malmö, Sweden</h2>
+        <h2 class="dot when">Together we connect the dots!</h2>
+        <p class="dot pretitle second">Knowit Festival is a conference for Knowit employees, by knowit employees.</p>
+        ${Link({
+          to: 'about',
+          className: 'button primary cfp border white',
+          content: `
+          ${buttonBackground}
+          ${buttonBackground}
+          ${buttonBackground}
+          ${buttonBackground}
+          ${buttonBackground}
+          ${translate('aboutKnowitFestival')}`
+        })}
       </div>
     </div>
   </section>`;
