c8daf90c23a17233a2b59ae5994c87525aa410a6
Karoline <karoline.vabo@knowit.no>
2025-05-09 12:47:46 +0200

    Changes on '/about' page

--- src/pages/AboutPage/index.ts
+++ src/pages/AboutPage/index.ts
@@ -1,50 +1,64 @@
 import { Link } from '../../components/navigation/Link';
 import { FAQ } from '../../components/page-section/FAQ';
+import buttonBackground from '../../assets/images/icons/extras/button.svg';
+import translate from '../../lib/translate';
+
 import './styles/about.styl';
 
 const html = String.raw;
 
 export const AboutPage = () => {
-  return html`<section class="dot section">
-    <div class="dot container">
-      <p class="dot about-leading-paragraph">
-      Knowit Festival, formerly known as Knowit Developer Summit (KDS), is a conference for Knowit employees, by Knowit employees. As KDS originally was an arena for the developers in Knowit, the new concept embraces the wide range of subject areas and expertise found in our employees, as well as underlying our need/mission/? for working interdisciplinary. Together we get it done!
-      </p>
-      <p class="dot">
-      ${Link({
-    to: 'https://www.knowit.eu/about-knowit/our-policies/',
-    server: true,
-    className: 'button primary coc',
-    content: 'Code of Conduct'
-  })}
-      </p>
-      <h2 class="dot about-title">
-      Knowit Festival 2025
-      </h2>
-      <p class="dot about-text>
-      In 2025, the conference will be in Sweden. The goal of the conference is to create a safe and low-threshold arena for our employees to share, learn and develop, as well as creating stronger bonds between our colleagues.
-      </p>
-      <p class="dot about-text>
-      The ambition for Knowit Festival is that it will be held annually and that its location varies from year to year.
-      </p>
-    <div class="dot container">
-      <h2 class="dot about-faq-title">FAQ</h2>
-      ${FAQ({
-    questions: [
-      {
-        q: 'Is the travel costs covered by the conference cost?',
-        a: 'The conference price does not cover travel, nor do we book for people.'
-      },
-      {
-        q: 'Where can I store my luggage?',
-        a: 'The conference facility will have an area where you can keep your luggage before hotel check-in and after hotel check-out. The hotel may also keep your luggage'
-      },
-      {
-        q: 'Which hotel am I staying at?',
-        a: 'TBD'
-      }
-    ]
-  })}
+return html`<section class="dot section">
+  <div class="dot container">
+    <h1 class="dot about-title">About the festival</h1>
+    <p class="dot about-leading-paragraph">
+    The ambition for Knowit Festival will be held annually and that its location varies from year to year.
+    </p>
+  </div>
+
+  <div class="dot container dot-history-container">
+    <div class="dot content">
+        <h2 class="dot history-title">History</h2>
+        <p class="dot history-text">
+          What started as a gathering for developers has grown into a celebration of all the amazing minds at Knowit. It’s more than just a conference. It’s a space that highlights our collaborative spirit and our commitment to working across disciplines to create impact.
+        </p>
+        <p class="dot history-text"> 
+        Together, we make it happen.
+        </p>
+          ${Link({
+      to: 'https://www.knowit.eu/about-knowit/our-policies/',
+      server: true,
+      className: 'button primary cfp border white history-button',
+      content: `
+      ${buttonBackground}
+      ${buttonBackground}
+      ${buttonBackground}
+      ${buttonBackground}
+      ${buttonBackground}
+      ${translate('codeOfConduct')}`
+    })}
     </div>
-  </section>`;
+  </div>
+
+  <div class="dot container">
+    <h2 class="dot about-faq-title">Frequently asked questions</h2>
+    ${FAQ({
+      questions: [
+        {
+          q: 'Is the travel costs covered by the conference cost?',
+          a: 'The conference price does not cover travel, nor do we book for people.'
+        },
+        {
+          q: 'Where can I store my luggage?',
+          a: 'The conference facility will have an area where you can keep your luggage before hotel check-in and after hotel check-out. The hotel may also keep your luggage'
+        },
+        {
+          q: 'Which hotel am I staying at?',
+          a: 'TBD'
+        }
+      ]
+    })}
+  </div>
+</section>
+`;
 };
